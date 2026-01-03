e81d3c39adc37ead9dd43fbb2d44ea5f72585f13
Drew Schatt <schatt@schatt.com>
2026-01-03 09:30:27 -0800

    Fix protocol implementation visibility
    - Made all protocol implementation methods and properties public in the InspectableView extension
    - Protocol implementations must match the visibility of the protocol (public)
    - This resolves compilation errors across all test platforms (iOS, macOS, tvOS, visionOS, watchOS)
    
    Protocol conformance is now properly implemented with correct access levels.

--- Development/Tests/Shared/TestHelpers/ViewInspectorWrapper.swift
+++ Development/Tests/Shared/TestHelpers/ViewInspectorWrapper.swift
@@ -18,36 +18,36 @@ import ViewInspector
 /// This allows the wrapper to return a consistent type regardless of platform
 /// Uses prefixed method names to avoid naming conflicts with ViewInspector and prevent infinite recursion
 public protocol Inspectable {
-    func sixLayerButton() throws -> Inspectable
-    func sixLayerText() throws -> Inspectable
-    func sixLayerText(_ index: Int) throws -> Inspectable
-    func sixLayerVStack() throws -> Inspectable
-    func sixLayerVStack(_ index: Int) throws -> Inspectable
-    func sixLayerHStack() throws -> Inspectable
-    func sixLayerHStack(_ index: Int) throws -> Inspectable
-    func sixLayerZStack() throws -> Inspectable
-    func sixLayerTextField() throws -> Inspectable
-    func sixLayerTextField(_ index: Int) throws -> Inspectable
-    func sixLayerLink() throws -> Inspectable
-    func sixLayerFindAllLinks() -> [Inspectable]
-    func sixLayerAccessibilityIdentifier() throws -> String
-    func sixLayerFindAll<T>(_ type: T.Type) -> [Inspectable]
-    func sixLayerFind<T>(_ type: T.Type) throws -> Inspectable
-    func sixLayerTryFind<T>(_ type: T.Type) -> Inspectable?
-    func sixLayerAnyView() throws -> Inspectable
-    func sixLayerString() throws -> String
-    func sixLayerCallOnTapGesture() throws
-    func sixLayerLabelView() throws -> Inspectable
-    func sixLayerTap() throws
-    var sixLayerCount: Int { get }
-    var sixLayerIsEmpty: Bool { get }
-    func sixLayerContains(_ element: Inspectable) -> Bool
+    public func sixLayerButton() throws -> Inspectable
+    public func sixLayerText() throws -> Inspectable
+    public func sixLayerText(_ index: Int) throws -> Inspectable
+    public func sixLayerVStack() throws -> Inspectable
+    public func sixLayerVStack(_ index: Int) throws -> Inspectable
+    public func sixLayerHStack() throws -> Inspectable
+    public func sixLayerHStack(_ index: Int) throws -> Inspectable
+    public func sixLayerZStack() throws -> Inspectable
+    public func sixLayerTextField() throws -> Inspectable
+    public func sixLayerTextField(_ index: Int) throws -> Inspectable
+    public func sixLayerLink() throws -> Inspectable
+    public func sixLayerFindAllLinks() -> [Inspectable]
+    public func sixLayerAccessibilityIdentifier() throws -> String
+    public func sixLayerFindAll<T>(_ type: T.Type) -> [Inspectable]
+    public func sixLayerFind<T>(_ type: T.Type) throws -> Inspectable
+    public func sixLayerTryFind<T>(_ type: T.Type) -> Inspectable?
+    public func sixLayerAnyView() throws -> Inspectable
+    public func sixLayerString() throws -> String
+    public func sixLayerCallOnTapGesture() throws
+    public func sixLayerLabelView() throws -> Inspectable
+    public func sixLayerTap() throws
+    public var sixLayerCount: Int { get }
+    public var sixLayerIsEmpty: Bool { get }
+    public func sixLayerContains(_ element: Inspectable) -> Bool
 }
 
 // Make InspectableView conform to our protocol
 // This extension applies to all InspectableView types from ViewInspector
 extension InspectableView: Inspectable {
-    func sixLayerButton() throws -> Inspectable {
+    public func sixLayerButton() throws -> Inspectable {
         // Use findAll(where:) to find buttons - Button types vary by label
         // Try to find any button by checking if we can access button-specific methods
         let allViews = try self.findAll(where: { view in
@@ -63,7 +63,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerButton() requires a Button view")
     }
     
-    func sixLayerText() throws -> Inspectable {
+    public func sixLayerText() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.Text to recursively search for Text views
         let texts = try self.findAll(ViewType.Text.self)
         if let firstText = texts.first {
@@ -72,7 +72,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerText() requires a Text view")
     }
     
-    func sixLayerText(_ index: Int) throws -> Inspectable {
+    public func sixLayerText(_ index: Int) throws -> Inspectable {
         // This works on VStack/HStack which have indexed access
         if let vStackSelf = self as? InspectableView<ViewType.VStack> {
             return try vStackSelf.text(index) as Inspectable
@@ -85,7 +85,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerText() requires a Text view")
     }
     
-    func sixLayerVStack() throws -> Inspectable {
+    public func sixLayerVStack() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.VStack to recursively search for VStacks
         // This will find VStacks even when wrapped in modifiers
         let vStacks = try self.findAll(ViewType.VStack.self)
@@ -95,7 +95,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerVStack() requires a VStack view")
     }
     
-    func sixLayerVStack(_ index: Int) throws -> Inspectable {
+    public func sixLayerVStack(_ index: Int) throws -> Inspectable {
         // VStack from MultipleViewContent parent (like another VStack) supports indexed access
         if let vStackSelf = self as? InspectableView<ViewType.VStack> {
             return try vStackSelf.vStack(index) as Inspectable
@@ -108,7 +108,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerVStack() requires a VStack view")
     }
     
-    func sixLayerHStack() throws -> Inspectable {
+    public func sixLayerHStack() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.HStack to recursively search for HStacks
         let hStacks = try self.findAll(ViewType.HStack.self)
         if let firstHStack = hStacks.first {
@@ -117,7 +117,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerHStack() requires an HStack view")
     }
     
-    func sixLayerHStack(_ index: Int) throws -> Inspectable {
+    public func sixLayerHStack(_ index: Int) throws -> Inspectable {
         // HStack from MultipleViewContent parent (like VStack) supports indexed access
         if let vStackSelf = self as? InspectableView<ViewType.VStack> {
             return try vStackSelf.hStack(index) as Inspectable
@@ -130,7 +130,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerHStack() requires an HStack view")
     }
     
-    func sixLayerZStack() throws -> Inspectable {
+    public func sixLayerZStack() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.ZStack to recursively search for ZStacks
         let zStacks = try self.findAll(ViewType.ZStack.self)
         if let firstZStack = zStacks.first {
@@ -139,7 +139,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerZStack() requires a ZStack view")
     }
     
-    func sixLayerTextField() throws -> Inspectable {
+    public func sixLayerTextField() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.TextField to recursively search for TextFields
         let textFields = try self.findAll(ViewType.TextField.self)
         if let firstTextField = textFields.first {
@@ -148,7 +148,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerTextField() requires a TextField view")
     }
     
-    func sixLayerTextField(_ index: Int) throws -> Inspectable {
+    public func sixLayerTextField(_ index: Int) throws -> Inspectable {
         // This works on VStack/HStack which have indexed access
         if let vStackSelf = self as? InspectableView<ViewType.VStack> {
             return try vStackSelf.textField(index) as Inspectable
@@ -161,7 +161,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerTextField() requires a TextField view")
     }
     
-    func sixLayerLink() throws -> Inspectable {
+    public func sixLayerLink() throws -> Inspectable {
         // Use ViewInspector's findAll with ViewType.Link to recursively search for Link views
         let links = try self.findAll(ViewType.Link.self)
         if let firstLink = links.first {
@@ -170,7 +170,7 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerLink() requires a Link view")
     }
     
-    func sixLayerFindAllLinks() -> [Inspectable] {
+    public func sixLayerFindAllLinks() -> [Inspectable] {
         // Use ViewInspector's findAll with ViewType.Link to recursively search for Link views
         // Non-throwing version - returns empty array on failure
         guard let links = try? self.findAll(ViewType.Link.self) else {
@@ -179,7 +179,7 @@ extension InspectableView: Inspectable {
         return links.map { $0 as Inspectable }
     }
     
-    func sixLayerAccessibilityIdentifier() throws -> String {
+    public func sixLayerAccessibilityIdentifier() throws -> String {
         // No recursion risk - different method name than ViewInspector's accessibilityIdentifier()
         // Only return the identifier for this specific view, do not search children recursively
         // If a test needs to find an identifier in a child view, it should navigate to that child first
@@ -193,7 +193,7 @@ extension InspectableView: Inspectable {
         return identifier
     }
     
-    func sixLayerFindAll<T>(_ type: T.Type) -> [Inspectable] {
+    public func sixLayerFindAll<T>(_ type: T.Type) -> [Inspectable] {
         // Use findAll(where:) to search for views matching the type
         // This works for all types but requires runtime type checking
         // Non-throwing version - returns empty array on failure
@@ -207,18 +207,18 @@ extension InspectableView: Inspectable {
         return allViews.map { $0 as Inspectable }
     }
     
-    func sixLayerFind<T>(_ type: T.Type) throws -> Inspectable {
+    public func sixLayerFind<T>(_ type: T.Type) throws -> Inspectable {
         // Thin wrapper around ViewInspector's find method
         // The slowness is in ViewInspector itself, not this wrapper
         let found = try self.find(where: { _ in true })
         return found as Inspectable
     }
     
-    func sixLayerTryFind<T>(_ type: T.Type) -> Inspectable? {
+    public func sixLayerTryFind<T>(_ type: T.Type) -> Inspectable? {
         return try? self.sixLayerFind(type)
     }
     
-    func sixLayerAnyView() throws -> Inspectable {
+    public func sixLayerAnyView() throws -> Inspectable {
         // Use find(where:) to find any view, avoiding SingleViewContent constraint
         // This is a workaround for the anyView() constraint issue
         if let anyViewResult = try? self.find(where: { _ in true }) {
@@ -228,7 +228,7 @@ extension InspectableView: Inspectable {
         return self as Inspectable
     }
     
-    func sixLayerString() throws -> String {
+    public func sixLayerString() throws -> String {
         // No recursion - different method name than ViewInspector's string()
         if let textView = self as? InspectableView<ViewType.Text> {
             return try textView.string()
@@ -236,12 +236,12 @@ extension InspectableView: Inspectable {
         throw InspectionError.notSupported("sixLayerString() can only be called on Text views")
     }
     
-    func sixLayerCallOnTapGesture() throws {
+    public func sixLayerCallOnTapGesture() throws {
         // No recursion - different method name than ViewInspector's callOnTapGesture()
         try self.callOnTapGesture()
     }
     
-    func sixLayerLabelView() throws -> Inspectable {
+    public func sixLayerLabelView() throws -> Inspectable {
         // Try to find text content - buttons typically have text labels
         // This is a simplified approach since we can't easily detect button types
         if let text = try? self.sixLayerFind(ViewType.Text.self) {
@@ -251,13 +251,13 @@ extension InspectableView: Inspectable {
         return self as Inspectable
     }
     
-    func sixLayerTap() throws {
+    public func sixLayerTap() throws {
         // Use callOnTapGesture for all tapable views
         // This works for buttons and other tappable views
         try self.callOnTapGesture()
     }
     
-    var sixLayerCount: Int {
+    public var sixLayerCount: Int {
         // No recursion - different property name than ViewInspector's count
         if let vStackSelf = self as? InspectableView<ViewType.VStack> {
             return vStackSelf.count
@@ -268,12 +268,12 @@ extension InspectableView: Inspectable {
         return 0
     }
     
-    var sixLayerIsEmpty: Bool {
+    public var sixLayerIsEmpty: Bool {
         // InspectableView doesn't have isEmpty directly
         return false
     }
     
-    func sixLayerContains(_ element: Inspectable) -> Bool {
+    public func sixLayerContains(_ element: Inspectable) -> Bool {
         // InspectableView doesn't have contains directly
         return false
     }
