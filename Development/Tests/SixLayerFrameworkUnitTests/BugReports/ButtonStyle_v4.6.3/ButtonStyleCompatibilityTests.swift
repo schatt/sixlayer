85306ba2c2cfe844eaddd8cc747dacc20939d578
Drew Schatt <schatt@schatt.com>
2026-08-16 17:13:55 -0700

    Remove tautological XCTAssertTrue(true) ButtonStyle test
    Availability is already proven by createAlertButton
    compiling in the remaining smoke tests.
    
    Refs #382

--- Development/Tests/SixLayerFrameworkUnitTests/BugReports/ButtonStyle_v4.6.3/ButtonStyleCompatibilityTests.swift
+++ Development/Tests/SixLayerFrameworkUnitTests/BugReports/ButtonStyle_v4.6.3/ButtonStyleCompatibilityTests.swift
@@ -35,28 +35,4 @@ final class ButtonStyleCompatibilityTests: XCTestCase {
         // Verify the button was created (basic smoke test)
         XCTAssertNotNil(destructiveButton)
     }
-    
-    func testButtonStyleAvailability() {
-        // Test that ButtonStyle APIs are available at runtime
-        // This will fail at compile time if APIs aren't available
-        
-        #if os(iOS)
-        if #available(iOS 15.0, *) {
-            // These should be available since we target iOS 16.0+
-            // Note: We can't test the actual ButtonStyle.bordered here due to compilation issues
-            // But the fact that our createAlertButton method compiles means the APIs are available
-            XCTAssertTrue(true, "ButtonStyle APIs should be available for iOS 15.0+")
-        }
-        #elseif os(macOS)
-        if #available(macOS 12.0, *) {
-            // These should be available since we target macOS 13.0+
-            // Note: We can't test the actual ButtonStyle.bordered here due to compilation issues
-            // But the fact that our createAlertButton method compiles means the APIs are available
-            XCTAssertTrue(true, "ButtonStyle APIs should be available for macOS 12.0+")
-        }
-        #endif
-        
-        // If we get here, the APIs are available
-        XCTAssertTrue(true, "ButtonStyle APIs should be available")
-    }
 }
