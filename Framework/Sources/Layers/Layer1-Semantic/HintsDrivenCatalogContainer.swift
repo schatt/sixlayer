7e7b37bec49466e0246e4e240ebb4fa27171ec50
Drew Schatt <schatt@schatt.com>
2026-09-11 14:52:21 -0700

    Make catalog viewport PreferenceKey default immutable
    Swift 6 rejects a mutable static defaultValue.
    
    Refs #477

--- Framework/Sources/Layers/Layer1-Semantic/HintsDrivenCatalogContainer.swift
+++ Framework/Sources/Layers/Layer1-Semantic/HintsDrivenCatalogContainer.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 private struct CatalogViewportWidthKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    static let defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = nextValue()
     }
